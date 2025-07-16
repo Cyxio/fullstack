470095839526a44d48778ac3ea28a2fc35eb813b
Risto <halmeristo10@gmail.com>
2025-07-16 19:48:13 +0300

    Osa3 tehty

---
+++ osa3/puhelinluettelo/index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+var morgan = require('morgan')
+
+const app = express();
+
+app.use(express.static('dist'))
+app.use(express.json());
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    req.body ? JSON.stringify(req.body) : '',
+  ].join(' ')
+}));
+
+const persons = [
+    { id: 1, name: 'Arto Hellas', number: '040-123456' },
+    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+    { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
+];
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const person = persons.find(p => p.id === id);
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).send('Person not found');
+    }
+});
+
+app.get('/api/info', (req, res) => {
+    const info = `Phonebook has info for ${persons.length} people`;
+    const date = new Date();
+    res.send(`${info}<br><br>${date}`);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const index = persons.findIndex(p => p.id === id);
+    if (index !== -1) {
+        persons.splice(index, 1);
+        res.status(204).end();
+    } else {
+        res.status(404).send('Person not found');
+    }
+});
+
+app.post('/api/persons', (req, res) => {
+    const {name, number} = req.body;
+    if (!name || !number) {
+        return res.status(400).send({'error': 'name and number are required'});
+    }
+    if (persons.some(p => p.name === name)) {
+        return res.status(400).send({'error': 'name must be unique'});
+    }
+    const id = Math.floor(Math.random() * 999999);
+    const newPerson = { id, name, number };
+    persons.push(newPerson);
+    res.status(201).json(newPerson);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
---
+++ osa3/puhelinluettelo_front/src/App.jsx
@@ -0,0 +1,130 @@
+import { useEffect, useState } from 'react'
+import { addPerson, getPersons, deletePerson, updatePerson } from './Actions'
+import './index.css'
+
+const Filter = ({ filter, setFilter }) => {
+  return (
+    <div>
+      filter shown with <input value={filter} onChange={(event) => setFilter(event.target.value)} />
+    </div>
+  )
+}
+
+const PersonForm = ({ newName, setNewName, newNumber, setNewNumber, handleSubmit }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({ persons, filter, deleteMethod }) => {
+  return (
+    <ul>
+      {persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase())).map((person, index) => (
+        <li key={index}>{person.name} {person.number} <button onClick={() => deleteMethod(person.name, person.id)}>delete</button></li>
+      ))}
+    </ul>
+  )
+}
+
+const App = () => {
+
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [error, setError] = useState(null)
+  const [success, setSuccess] = useState(null)
+
+  const clearMessages = () => {
+    setTimeout(() => {
+      setError(null)
+      setSuccess(null)
+    }, 3000)
+  }
+
+  const handleSubmit = (event) => {
+      event.preventDefault()
+      const personObject = {
+        name: newName,
+        number: newNumber
+      }
+      if (persons.some(person => person.name === newName) && window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        updatePerson(persons.find(person => person.name === newName).id, personObject)
+          .then(data => {
+            setSuccess(`Updated ${data.name}`)
+            clearMessages()
+            setPersons(persons.map(person => person.id !== data.id ? person : data))
+          }).catch(error => {
+            setError(`Information of ${newName} has already been removed from server`)
+            clearMessages()
+          })
+        setNewName('')
+        setNewNumber('')
+        return
+      }
+
+      addPerson(personObject).then(data => {
+        setSuccess(`Added ${data.name}`)
+        clearMessages()
+        setPersons(persons.concat(data))
+      }).catch(error => {
+        setError(`Error adding person: ${error}`)
+        clearMessages()
+      })
+      setNewName('')
+      setNewNumber('')
+  }
+
+  useEffect(() => {
+    getPersons().then(data => {
+      setPersons(data)
+    })
+  }, [])
+
+  const deleteMethod = (name, id) => {
+    if (window.confirm(`Delete ${name}?`)) {
+      const updatedPersons = persons.filter(person => person.id !== id)
+      deletePerson(id).then(() => {
+        setSuccess(`Deleted ${name}`)
+        clearMessages()
+        setPersons(updatedPersons)
+      }).catch(error => {
+        setError(`Error deleting ${name}: ${error}`)
+        clearMessages()
+        setPersons(persons)
+      })
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      {success && <div className="success">{success}</div>}
+      {error && <div className="error">{error}</div>}
+      <Filter filter={filter} setFilter={setFilter} />
+      <h3>Add a new</h3>
+      <PersonForm 
+        newName={newName} 
+        setNewName={setNewName} 
+        newNumber={newNumber} 
+        setNewNumber={setNewNumber} 
+        handleSubmit={handleSubmit}
+      />
+      <h3>Numbers</h3>
+      <Persons persons={persons} filter={filter} deleteMethod={deleteMethod} />
+    </div>
+  )
+
+}
+
+export default App
